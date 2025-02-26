57e14800f9528cce9e9dd4a4a9f3f854b310dc87
amirhosseinbt <amirhosseinbrbt@gmail.com>
2025-02-26 19:18:00 +0330

    connect to back end

--- src/signIn/SignIn.js
+++ src/signIn/SignIn.js
@@ -1,7 +1,66 @@
-import React from "react";
+import {React,useState} from "react";
+import axios from "axios";
 import Logo from '../assets/media/logos/custom-2.svg'
 import './SignIn.css'
 const SignIn = ()=> {
+   const [phoneNumber , setPhoneNumber] = useState('');
+   const [password , setPassword] =useState('');
+
+   const onPhoneNumberChange = (event) => {
+      setPhoneNumber(event.target.value);
+  }
+  const onPasswordChange = (event) => {
+      setPassword(event.target.value);
+  }
+   const onSubmitSignin = () =>{
+      axios
+      .post("http://192.168.11.185:8000/user/login/", {
+          phone : phoneNumber,
+          password : password,
+      })
+      .then((res) => {
+          this.setState({
+              user: "",
+              quote: "",
+          });
+      })
+      .catch((err) => {console.log(err)});
+      // fetch('http://192.168.11.185:8000/user/login/',{
+      //     method:'POST',
+      //     headers:{'Content-Type':'application/json'},
+      //     body: JSON.stringify(
+      //         {
+      //             phone : phoneNumber,
+      //             password : password
+      //         }
+              
+      //     )
+      // })
+      // .then(response => console.log(response.json()))
+      // .then(data => {
+      //     if(data.id)
+      //     {
+      //         const user = {
+      //             username:data.username,
+      //             entries:data.entries
+      //         }
+      //         localStorage.setItem('user',JSON.stringify(user));
+      //         setshowalert(false);
+      //         navigate('/');
+      //     }
+      //     else 
+      //     {
+      //         setshowalert(true);
+              
+      //         setalerttext('نام کاربری یا رمز عبور اشتباه است.');
+      //     }
+      // })
+      // .catch(err => {
+      //     setshowalert(true);
+      //     setalerttext('دوباره تلاش کنید.'); 
+      // })
+      // setshowalert(false);
+  }
     return(
       <div className="d-flex flex-column flex-root">
       <div className="d-flex flex-column flex-column-fluid flex-lg-row">
@@ -23,12 +82,15 @@ const SignIn = ()=> {
                      </div>
 
                      <div className="fv-row mb-6">
-                        <input placeholder="شماره تلفن همراه" name="phoneNumber" type="tel" autoComplete="off" className="form-control bg-transparent" dir="rtl" />
+                        <input placeholder="شماره تلفن همراه" name="phoneNumber" type="tel" autoComplete="off" className="form-control bg-transparent" dir="rtl" onChange={onPhoneNumberChange} />
                      </div>
                      <div className="text-muted mb-4" dir="rtl">تلفن همراه باید شامل یازده عدد باشد.</div>
+                     <div className="fv-row mb-6">
+                        <input placeholder="رمز عبور" name="phoneNumber" type="password" autoComplete="off" className="form-control bg-transparent" dir="rtl" onChange={onPasswordChange}/>
+                     </div>
 
                      <div className="d-grid mb-10">
-                        <button type="submit" id="kt_sign_up_submit" className="btn btn-primary">
+                        <button type="button" id="kt_sign_up_submit" className="btn btn-primary" onClick={onSubmitSignin}>
                            <span className="indicator-label">ورود</span>
                            <span className="indicator-progress">Please wait...
                            <span className="spinner-border spinner-border-sm align-middle ms-2"></span></span>
